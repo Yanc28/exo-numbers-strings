8a1add1cc79235865a2e801570b4b47f5cc97b14
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-17 16:33:18 +0100

    done

--- showChar.js
+++ showChar.js
@@ -1,11 +1,21 @@
-let text = `C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar.`
 
-for (let c = 0; c < text.length; ++c) {
-  console.log(`character ${text[c]} is at index: ${c}`)
-}
+
+let text = `C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar.`
+let auteur = `Gustave Flaubert`
+let livre = `Salammbô`
+console.log(`Ce texte ${text} est extrait du livre ${livre} de ${auteur}, et contient ${text.length} characters.`)
+
 
 
 // programme that shows line by line what index is attach to each character
+for (let i = 0; i < text.length; ++i) {
+  console.log(`character ${text[i]} is at index: ${i}`)
+}
+
+
+
+
+
 
 
 
