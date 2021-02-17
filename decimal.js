7322622c72df441aa9ab08757abf460a36fec934
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-17 09:48:08 +0100

    done

---
+++ decimal.js
@@ -0,0 +1,17 @@
+let nb1 = 0x123
+let nb2 = 0123
+let nb3 = 0b10011001
+let nb4 = 0xdeadbeef
+let nb5 = 0xea7beef
+let nb6 = 0b1111111111111111
+let nb7 = 0777
+
+console.log(nb1.toString()) // output : 291
+console.log(nb2.toString()) // output : 83 
+console.log(nb3.toString()) // output : 153 
+console.log(nb4.toString()) // output : 3735928559
+console.log(nb5.toString()) // output : 245874415
+console.log(nb6.toString()) // output : 65535
+console.log(nb7.toString()) // output : 511
+
+// programme qui affiche sur la console les nombres en base 10 //
