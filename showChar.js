d05443f13fe1df1b378900f03859418bcdc6911c
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-17 15:04:29 +0100

    donebymyself

---
+++ showChar.js
@@ -0,0 +1,13 @@
+let text = `C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar.`
+
+for (let c = 0; c < text.length; ++c) {
+  console.log(`character ${text[c]} is at index: ${c}`)
+}
+
+
+// programme that shows line by line what index is attach to each character
+
+
+
+
+
