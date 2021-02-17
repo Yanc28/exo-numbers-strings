a587ab1ca1a29c4fe1a0592a552c48daf6c1903e
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-17 12:11:37 +0100

    correction done

---
+++ convert.js
@@ -0,0 +1,13 @@
+let nb = 10
+console.log(`For the number ${nb}, \n In binary code : ${nb.toString(2)} \n In octal code : ${nb.toString(8)} \n In hexadecimal code : ${nb.toString(16)} \n`)
+let nb1 = 15
+console.log(`For the number ${nb1}, \n In binary code : ${nb1.toString(2)} \n In octal code : ${nb1.toString(8)} \n In hexadecimal code : ${nb1.toString(16)} \n`)
+let nb2 = 16
+console.log(`For the number ${nb2}, \n In binary code : ${nb2.toString(2)} \n In octal code : ${nb2.toString(8)} \n In hexadecimal code : ${nb2.toString(16)} \n`)
+let nb3 = 5005
+console.log(`For the number ${nb3}, \n In binary code : ${nb3.toString(2)} \n In octal code : ${nb3.toString(8)} \n In hexadecimal code : ${nb3.toString(16)} \n`)
+let nb4 = 52390903
+console.log(`For the number ${nb4}, \n In binary code : ${nb4.toString(2)} \n In octal code : ${nb4.toString(8)} \n In hexadecimal code : ${nb4.toString(16)} \n`)
+
+
+
